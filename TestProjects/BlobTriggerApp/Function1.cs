2dc9a769f3c9b1885eba2e8c649b298521c56600
Kroy <jimmy.kroy1976@gmail.com>
2025-12-02 22:15:47 +0100

    Added BlobTriggerApp

---
+++ TestProjects/BlobTriggerApp/Function1.cs
@@ -0,0 +1,25 @@
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace BlobTriggerApp;
+
+public class Function1
+{
+    private readonly ILogger<Function1> _logger;
+
+    public Function1(ILogger<Function1> logger)
+    {
+        _logger = logger;
+    }
+
+    [Function(nameof(Function1))]
+    public async Task Run([BlobTrigger("jokes-container/{name}", Connection = "AzureWebJobsStorage")] Stream stream, string name)
+    {
+        _logger.LogInformation("Blob trigger activated.");
+        using var blobStreamReader = new StreamReader(stream);
+        var content = await blobStreamReader.ReadToEndAsync();
+        _logger.LogInformation("C# Blob trigger function Processed blob\n Name: {name} \n Data: {content}", name, content);
+    }
+}
