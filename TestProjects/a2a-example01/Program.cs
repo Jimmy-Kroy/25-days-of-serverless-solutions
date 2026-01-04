76a3865182d2a46c17ddce9a7a14c137a62243a5
Kroy <jimmy.kroy1976@gmail.com>
2026-01-04 16:53:32 +0100

    Updated a2a server app

--- TestProjects/a2a-example01/Program.cs
+++ TestProjects/a2a-example01/Program.cs
@@ -1,30 +1,59 @@
 /*
 
-
-
-    The weatherforecast service can be accessed via the url: https://localhost:7114/weatherforecast
+    The swagger service can be accessed via the url: https://localhost:7114/swagger/index.html
 */
+
+
+
+using A2A.AspNetCore;
+using Azure.AI.OpenAI;
+using Azure.Identity;
+using Microsoft.Agents.AI.Hosting;
+using Microsoft.Extensions.AI;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
 
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi(); 
+string endpoint = builder.Configuration["AZURE_OPENAI_ENDPOINT"]
+    ?? throw new InvalidOperationException("AZURE_OPENAI_ENDPOINT is not set.");
+string deploymentName = builder.Configuration["AZURE_OPENAI_DEPLOYMENT_NAME"]
+    ?? throw new InvalidOperationException("AZURE_OPENAI_DEPLOYMENT_NAME is not set.");
+
+Console.WriteLine($"endpoint: {endpoint}, deploymentName: {deploymentName}");
+
+// Register the chat client
+//IChatClient chatClient = new AzureOpenAIClient(
+//        new Uri(endpoint),
+//        new DefaultAzureCredential())
+//    .GetChatClient(deploymentName)
+//    .AsIChatClient();
+
+AzureOpenAIClient azureClient = new(
+    new Uri(endpoint),
+    new AzureCliCredential());
+
+IChatClient chatClient = azureClient.GetChatClient(deploymentName).AsIChatClient();
+
+builder.Services.AddSingleton(chatClient);
+
+// Register an agent
+var pirateAgent = builder.AddAIAgent("pirate", instructions: "You are a pirate. Speak like a pirate.");
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.MapOpenApi();
+app.UseSwagger();
+app.UseSwaggerUI();
+
+// Expose the agent via A2A protocol. You can also customize the agentCard
+app.MapA2A(pirateAgent, path: "/a2a/pirate", agentCard: new()
 {
-    app.MapOpenApi();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
+    Name = "Pirate Agent",
+    Description = "An agent that speaks like a pirate.",
+    Version = "1.0"
+});
 
 app.Run();
